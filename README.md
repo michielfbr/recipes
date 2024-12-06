1c51a81d6b61e52ff8af936eca9cefcf7190dc6f
Michiel Brongers <michiel.brongers@cryptotrader.org>
2024-12-06 10:49:08 +0100

    Create Phoenix app

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 A tool to save, edit and browse through the recipes you collect.
 
 This is a rebuild of the React/Express JS project I built years ago.
+
+To start your Phoenix server:
+
+- Run `mix setup` to install and setup dependencies
+- Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
+
+Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+Ready to run in production? Please [check our deployment guides](https://hexdocs.pm/phoenix/deployment.html).
+
+## Learn more
+
+- Official website: https://www.phoenixframework.org/
+- Guides: https://hexdocs.pm/phoenix/overview.html
+- Docs: https://hexdocs.pm/phoenix
+- Forum: https://elixirforum.com/c/phoenix-forum
+- Source: https://github.com/phoenixframework/phoenix
---
+++ lib/recipes/application.ex
@@ -0,0 +1,36 @@
+defmodule Recipes.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      RecipesWeb.Telemetry,
+      Recipes.Repo,
+      {DNSCluster, query: Application.get_env(:recipes, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Recipes.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: Recipes.Finch},
+      # Start a worker by calling: Recipes.Worker.start_link(arg)
+      # {Recipes.Worker, arg},
+      # Start to serve requests, typically the last entry
+      RecipesWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Recipes.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    RecipesWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
