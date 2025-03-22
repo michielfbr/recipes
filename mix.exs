ba728d6c9c38ba04024fbe59d75c148b50e2c3da
Michiel Brongers <michiel.brongers@cryptotrader.org>
2025-03-22 13:26:19 +0100

    Updrage to Elixir v1.18.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Recipes.MixProject do
     [
       app: :recipes,
       version: "0.1.0",
-      elixir: "~> 1.17.3",
+      elixir: "~> 1.18.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
