6cff3d0f061625c77f77634249357864a08f9f71
Michiel <michiel@valkdigital.nl>
2025-12-24 15:12:00 +0100

    re-generate auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :recipes, :scopes,
+  user: [
+    default: true,
+    module: Recipes.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Recipes.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :recipes,
   ecto_repos: [Recipes.Repo],
   generators: [timestamp_type: :utc_datetime]
--- lib/recipes_web/router.ex
+++ lib/recipes_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RecipesWeb.Router do
   use RecipesWeb, :router
 
+  import RecipesWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RecipesWeb.Router do
     plug :put_root_layout, html: {RecipesWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule RecipesWeb.Router do
       live_dashboard "/dashboard", metrics: RecipesWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", RecipesWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{RecipesWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", RecipesWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{RecipesWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
